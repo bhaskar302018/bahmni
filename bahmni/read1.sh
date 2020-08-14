97d904597eff5334db3a18306f298068558140d5
bhaskar <bhaskar302018@gmail.com>
2020-08-14 15:57:23 +0530

    read1 for service start

---
+++ bahmni/read1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo service mysqld start
+sudo service postgresql-9.6 start
+sudo service openmrs start
+sudo service httpd start
+sudo service bahmni-reports start
+sudo service bahmni-lab start
+sudo service odoo start
+sudo service bahmni-erp-connect start
+sudo service atomfeed-console start
+sudo service pacs-integration start
